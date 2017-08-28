cf53482c3b92d1dbe938bec964d8f8b2c3edea48
Rajab Natshah <rajabn@gmail.com>
2017-08-28 10:05:53 +0300

    Issue #2904173: Added Validation for Available background colors for Varbase Bootstrap Paragraphs settings form.

--- src/Form/VarbaseBootstrapParagraphsSettingsForm.php
+++ src/Form/VarbaseBootstrapParagraphsSettingsForm.php
@@ -4,6 +4,7 @@ namespace Drupal\varbase_bootstrap_paragraphs\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Unicode;
 
 /**
  * Provides form for managing module settings.
@@ -38,7 +39,7 @@ class VarbaseBootstrapParagraphsSettingsForm extends ConfigFormBase {
       '#cols' => 60,
       '#rows' => 10,
     ];
-
+    
 
     return parent::buildForm($form, $form_state);
   }
@@ -52,6 +53,66 @@ class VarbaseBootstrapParagraphsSettingsForm extends ConfigFormBase {
     $config->save();
 
     parent::submitForm($form, $form_state);
+  }
+  
+  /**
+   * @inheritdoc
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    
+    $values = self::optionsExtractAllowedListTextValues($form_state->getValue('background_colors'));
+
+    if (!is_array($values)) {
+      $form_state->setErrorByName('background_colors', t('Allowed values list: invalid input.'));
+    }
+    else {
+      // Check that keys are valid for the field type.
+      foreach ($values as $key => $value) {
+        if (Unicode::strlen($key) > 255) {
+          $form_state->setErrorByName('background_colors', t('Allowed values list: each key must be a string at most 255 characters long.'));
+          break;
+        }
+      }
+    }
+  }
+  
+  /**
+   * Parses a string of 'allowed values' into an array.
+   *
+   * @param $string
+   *   The list of allowed values in string format described in
+   *   optionsExtractAllowedValues().
+   *
+   * @return
+   *   The array of extracted key/value pairs, or NULL if the string is invalid.
+   *
+   * @see optionsExtractAllowedListTextValues()
+   */
+  public function optionsExtractAllowedListTextValues($string) {
+    $values = array();
+
+    $list = explode("\n", $string);
+    $list = array_map('trim', $list);
+    $list = array_filter($list, 'strlen');
+
+    foreach ($list as $position => $text) {
+      $value = $key = FALSE;
+
+      // Check for an explicit key.
+      $matches = array();
+      if (preg_match('/(.*)\|(.*)/', $text, $matches)) {
+        // Trim key and value to avoid unwanted spaces issues.
+        $key = trim($matches[1]);
+        $value = trim($matches[2]);
+        $values[$key] = $value;
+      }
+      else {
+        return;
+      }
+    }
+
+    return $values;
   }
 
 }
