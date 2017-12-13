09b77fab762b1f300dfe2cd044e1e51f00b58f23
Rajab Natshah <rajabn@gmail.com>
2017-12-13 14:54:05 +0200

    Issue #2928269: Changed the UI/UX for [Styling Settings] of paragraphs at the back-end form display

--- js/vbp-scripts.admin.js
+++ js/vbp-scripts.admin.js
@@ -11,6 +11,31 @@
       $(".field--name-bp-background.field--widget-options-buttons input:radio").each(function() {
         $(this).next('label').addClass($(this).val());
       });
+      
+      $(".field--name-bp-width.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass($(this).val());
+      });
+      
+      $(".field--name-bp-gutter.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass('gutter-' + $(this).val());
+      });
+      
+      $(".field--name-bp-column-style-3.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass($(this).val());
+      });
+      
+      $(".field--name-bp-column-style-2.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass($(this).val());
+      });
+      
+      $(".field--name-text-and-image-style.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass($(this).val());
+      });
+
+      $(".field--name-field-image-position.field--widget-options-buttons input:radio").each(function() {
+        $(this).next('label').addClass($(this).val());
+      });
+
     }
   };
 
